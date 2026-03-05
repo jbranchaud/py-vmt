3e31bf2b785d674357b3cf59858013f62dba7ae6
jbranchaud <jbranchaud@gmail.com>
2026-03-05 17:50:44 -0600

    Print out flag argument details

--- src/py_vmt/cli.py
+++ src/py_vmt/cli.py
@@ -13,6 +13,8 @@ def cli():
 @click.option("--at", help='Hours previous to start the timer, e.g. "2 hours ago"')
 def start(project_name: str, at: Optional[str] = None):
     print(f"Starting a time for project {project_name}")
+    if at:
+        print(f"  with flag --at of '{at}'")
 
 
 @cli.command()
