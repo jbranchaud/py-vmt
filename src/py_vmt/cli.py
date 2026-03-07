9958ee65c09c239577d3658721f538ec57e0b743
jbranchaud <jbranchaud@gmail.com>
2026-03-07 10:19:49 -0600

    Use `click.echo` instead of `print` as recommended by click docs
    https://click.palletsprojects.com/en/stable/api/#click.echo

--- src/py_vmt/cli.py
+++ src/py_vmt/cli.py
@@ -35,15 +35,15 @@ def cli(ctx):
 @click.option("--at", help='Hours previous to start the timer, e.g. "2 hours ago"')
 @click.pass_context
 def start(ctx, project_name: str, at: Optional[str] = None):
-    print(
+    click.echo(
         f"start cmd ctx - data_dir: {ctx.obj.data_dir}, config_dir: {ctx.obj.config_dir}"
     )
-    print(f"Starting a time for project {project_name}")
+    click.echo(f"Starting a time for project {project_name}")
     if at:
-        print(f"  with flag --at of '{at}'")
+        click.echo(f"  with flag --at of '{at}'")
 
 
 @cli.command()
 @click.option("--at", help='Hours previous to end the timer, e.g. "2 hours ago"')
 def stop(at: Optional[str] = None):
-    print("Stopping the timer for the current project")
+    click.echo("Stopping the timer for the current project")
