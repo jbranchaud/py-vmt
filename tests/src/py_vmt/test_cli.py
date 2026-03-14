55247dcbbbb83df9ebbc3ef62fbe110541422fa4
jbranchaud <jbranchaud@gmail.com>
2026-03-14 15:30:36 -0500

    Run tests in isolated filesystem and freeze time
    Freezing time allows for more easily writing tests that assert specific
    time-based behavior like the time session tracking started

--- tests/src/py_vmt/test_cli.py
+++ tests/src/py_vmt/test_cli.py
@@ -1,24 +1,33 @@
 from click.testing import CliRunner
+import datetime
+from freezegun import freeze_time
 from py_vmt.cli import cli
 
 
 def test_no_status():
     runner = CliRunner()
-    result = runner.invoke(cli, ["status"])
-    assert "Not tracking" in result.output
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["status"])
+        assert "Not tracking" in result.output
 
 
 def test_active_status():
     runner = CliRunner()
 
-    # start a session
-    start_result = runner.invoke(cli, ["start", "my-project"])
-    assert "Started tracking 'my-project'" in start_result.output
+    with runner.isolated_filesystem():
+        initial_datetime = datetime.datetime(
+            2026, 3, 14, 15, 5, 11, 0, datetime.timezone.utc
+        )
+        with freeze_time(initial_datetime) as frozen_datetime:
+            # start a session
+            start_result = runner.invoke(cli, ["start", "my-project"])
+            output = "Started tracking 'my-project' at 10:05AM"
+            assert output in start_result.output
 
-    # check status
-    status_result = runner.invoke(cli, ["status"])
-    assert "Tracking 'my-project'" in status_result.output
+            # check status
+            status_result = runner.invoke(cli, ["status"])
+            assert "Tracking 'my-project'" in status_result.output
 
-    # stop a session
-    stop_result = runner.invoke(cli, ["stop"])
-    assert "Stopped tracking 'my-project'" in stop_result.output
+            # stop a session
+            stop_result = runner.invoke(cli, ["stop"])
+            assert "Stopped tracking 'my-project'" in stop_result.output
