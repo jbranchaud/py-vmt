de547ed56cfc7f5992d9a0c7c9499025b38c220b
jbranchaud <jbranchaud@gmail.com>
2026-05-01 21:40:35 -0500

    Write denser tests with pytest parametrize decorator

--- tests/src/py_vmt/test_time_helpers.py
+++ tests/src/py_vmt/test_time_helpers.py
@@ -2,25 +2,13 @@ import pytest
 from datetime import timedelta
 from py_vmt.time_helpers import format_time_delta
 
-def test_format_time_delta():
-    # less than a minute
-    thirty_seconds = timedelta(seconds=30)
-    assert "30s" == format_time_delta(thirty_seconds)
-
-    # one minute exactly
-    one_minute = timedelta(seconds=60)
-    assert "1m" == format_time_delta(one_minute)
-
-    # more than a minute
-    assert "1m30s" == format_time_delta(one_minute + thirty_seconds)
-
-    # bunch of minutes and seconds
-    delta = timedelta(minutes=24, seconds=8)
-    assert "24m8s" == format_time_delta(delta)
-
-    # one hour exactly
-    one_hour = timedelta(hours=1)
-    assert "1h" == format_time_delta(one_hour)
-
-    # more than one hour
-    assert "1h24m" == format_time_delta(one_hour + delta)
+@pytest.mark.parametrize("input,expected", [
+    (timedelta(seconds=30), "30s"),
+    (timedelta(seconds=60), "1m"),
+    (timedelta(seconds=90), "1m30s"),
+    (timedelta(minutes=24, seconds=8), "24m8s"),
+    (timedelta(hours=1), "1h"),
+    (timedelta(hours=1, minutes=24, seconds=8), "1h24m"),
+])
+def test_format_time_delta(input, expected):
+    assert format_time_delta(input) == expected
